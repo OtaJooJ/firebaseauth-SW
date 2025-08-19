685993cb0591aa033ee444f7c48fc2eacc4c745e
OtaJooJ <otavio.roque@etec.sp.gov.br>
2025-08-19 08:54:40 -0300

    push quase finalizado

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,10 +1,17 @@
 // Importa as funções necessárias do firebase
-import { initializeApp } from "";
-import { getAuth, GoogleProvider, signInWithPopup, signOut, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "";
-import { getFirestore, setDoc, doc } from "";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth, GoogleProvider, signOut, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore, getDoc, doc } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 // Configuração do Firebase
-
+const firebaseConfig = {
+    apiKey: "AIzaSyAZIJFAxszHUdXlbRII4hw7ntTju32XuF0",
+    authDomain: "fir-auth-dd1a8.firebaseapp.com",
+    projectId: "fir-auth-dd1a8",
+    storageBucket: "fir-auth-dd1a8.firebasestorage.app",
+    messagingSenderId: "218880570359",
+    appId: "1:218880570359:web:66f2b93ffe6c74801db4f2"
+  };
 
 // Inicializa o firebase
 const app = initializeApp(firebaseConfig);
@@ -37,7 +44,7 @@ signUp.addEventListener("click", (event) => {
     // Cria uma conta com e-mail e senha
     createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
         const user = userCredential.user; //Usuário Autenticado
-        const userData = { email, firstName, LastName}; // Dados do usuário para salvar
+        const userData = { email, firstName, lastName}; // Dados do usuário para salvar
 
         showMessage("Conta criada com sucesso", "signUpMessage"); // Exibe mensagem de sucesso
 
@@ -61,16 +68,33 @@ signUp.addEventListener("click", (event) => {
     });
 });
 
-// Lógica de login de usuários
+// Lógica de login de usuários existentes
 
 const signIn = document.getElementById("submitSignIn");
 signIn.addEventListener("click", (event) => {
     event.preventDefault(); // Previne o comportamento padrão do botão
 
     // Adiciona os dados do formulario de login
-    const email = document.getElementById("rEmail").value;
-    const password = document.getElementById("rPassword").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
     const auth = getAuth(); // Configura o serviço de autenticação
 
     // Realiza o login com e-mail e senha
-})
+    signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        showMessage("Usuário logado com sucesso", "signInMessage"); // Exibe mensagem de sucesso
+        const user = userCredential.user;
+
+        // Salva o ID do usuário no localStorage
+        localStorage.setItem("loggedInUserId", user.uid);
+
+        window.location.href = "homepage.html"; //Redireciona para a página inicial
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        if (errorCode === "auth/invalid-credential") {
+            showMessage("Email ou Senha incorreta", "signInMessage");
+        } else {
+            showMessage("Essa conta não existe", "signInMessage")
+        }
+    });
+});
---
+++ homepage.js
@@ -0,0 +1,62 @@
+// Importa as funções necessárias do firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth, GoogleProvider, signOut, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore, getDoc, doc } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+// Configuração do Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAZIJFAxszHUdXlbRII4hw7ntTju32XuF0",
+    authDomain: "fir-auth-dd1a8.firebaseapp.com",
+    projectId: "fir-auth-dd1a8",
+    storageBucket: "fir-auth-dd1a8.firebasestorage.app",
+    messagingSenderId: "218880570359",
+    appId: "1:218880570359:web:66f2b93ffe6c74801db4f2"
+  };
+
+// Inicializa o firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(); // Configura o firebase Authentication
+const dbo = getFirestore(); // Configura o firestore
+
+// Monitora o estado de autenticação do usuário
+onAuthStateChanged(auth, (user) => {
+    // Busca o ID do usuário autenticado salvo no localStorage
+    const loggedInUserId = localStorage.getItem("loggenInUserId");
+    
+    // Se o ID estiver no localSotrage, tenta obter os dados do Firestore
+    if (loggedInUserId) {
+        console.log(user);
+        const docRef = doc(db, "users", loggedInUserId); // Referência ao documento do usuário no Firestore
+
+        getDoc(docRef) // Busca o documento
+        .then((docSnap) => {
+            // Se o documento existir, exibe os dados na interface
+            if (docSnap.exists()) {
+                const userData = docSnap.data;
+                document.getElementById("loggedUserFName").innerText = userData.firstName;
+                document.getElementById("loggedUserLName").innerText = userData.lastName;
+                document.getElementById("loggedUserEmail").innerText = userData.email;
+            } else {
+                console.log("ID não encontrado no Documento");
+            }
+        })
+        .catch((error) => {
+            console.log("Documento não encontrado");
+        });
+    } else {
+        console.log("ID de usuário não encontrado no localStorage");
+    }
+});
+
+// Lógica de Logout
+const logoutButton = document.getElementById("logout");
+logoutButton.addEventListener("click", () => {
+    localStorage.removeItem("loggedInUserId"); // Remove o ID do LocalStorage
+    signOut(auth) //Realiza logout
+    .then(() => {
+        window.location.href = "index.html"; // Redireciona para a página de login
+    })
+    .catch((error) => {
+        console.error("Error Signing Out:", error);
+    });
+});
